3903c2d6ba6653ee04dfdd26c331c5a1dbb7389f
ysj1173886760 <1173886760@qq.com>
2021-07-25 19:02:53 +0800

    update graphics

---
+++ graphics/Assignment1/┤·┬δ┐≥╝▄/build/CMakeFiles/Rasterizer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  "CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "Rasterizer.pdb"
+  "Rasterizer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rasterizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
