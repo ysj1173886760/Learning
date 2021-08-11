d7dec12f31350fc5244b13f19d27df4a4a64f585
ysj1173886760 <1173886760@qq.com>
2021-08-11 10:39:33 +0800

    implement basic path tracing

--- graphics/PA7/Assignment7/Scene.cpp
+++ graphics/PA7/Assignment7/Scene.cpp
@@ -60,5 +60,43 @@ bool Scene::trace(
 // Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
+    if (depth > maxDepth)
+        return Vector3f(0.0, 0.0, 0.0);
+
+    Intersection intec = intersect(ray);
+    Vector3f hitColor = this->backgroundColor;
+    Material *m = intec.m;
+    Vector3f normal = intec.normal;
+
+    if (intec.happened) {
+        Intersection pos;
+        float pdf;
+        auto L_dir = Vector3f(0.0, 0.0, 0.0);
+        auto L_indir = Vector3f(0.0, 0.0, 0.0);
+        sampleLight(pos, pdf);
+
+        auto ws = (pos.coords - intec.coords).normalized();
+        bool notBlocked = (intersect(Ray(intec.coords, ws)).coords - pos.coords).norm() < EPSILON;
+        auto dist = pow((pos.coords - intec.coords).norm(), 2);
+
+        if (notBlocked) {
+            L_dir = pos.emit * m->eval(ray.direction, ws, normal) * dotProduct(ws, normal) * dotProduct(-ws, pos.normal) / pdf / dist;
+        }
+
+        if (get_random_float() <= RussianRoulette) {
+            auto wi = m->sample(ray.direction, normal);
+
+            Ray ray2(intec.coords, wi);
+            Intersection pos2 = intersect(ray2);
+
+            if (pos2.happened) {
+                L_indir = castRay(ray2, depth + 1) * m->eval(ray.direction, wi, normal) * dotProduct(wi, normal) / m->pdf(ray.direction, wi, normal) / RussianRoulette;
+            }
+        }
+
+        hitColor = L_dir + L_indir;
+    }
+
+    return hitColor;
     // TO DO Implement Path Tracing Algorithm here
 }
