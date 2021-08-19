18991768cb9dad3813f843f0b566237e3dd85636
ysj1173886760 <1173886760@qq.com>
2021-08-19 16:58:25 +0800

    implement moving

--- sfml/sprite_test/include/player.h
+++ sfml/sprite_test/include/player.h
@@ -5,6 +5,7 @@ public:
     Player(sf::Window *window): _window(window), _shape(sf::Vector2f(64, 64)) {
         _shape.setOrigin(32, 32);
         _shape.setPosition(300, 300);
+        _speed = 0.0002;
     }
 
     Player(const Player &) = delete;
@@ -16,7 +17,7 @@ public:
 private:
     virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
     sf::RectangleShape _shape;
-    sf::Vector2f _velocity;
     sf::Window *_window;
+    float _speed;
     sf::Texture texture;
 };
--- sfml/sprite_test/src/player.cpp
+++ sfml/sprite_test/src/player.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include "player.h"
 
+const float COS45 = 0.707106f;
+
 void Player::update(const sf::Time &deltaTime) {
     sf::Vector2i mousePos = sf::Mouse::getPosition(*_window);
     sf::Vector2f currentPos = _shape.getPosition();
@@ -11,6 +13,24 @@ void Player::update(const sf::Time &deltaTime) {
 
     auto angle = atan2(deltaY, deltaX) / M_PI * 180;
     _shape.setRotation(angle);
+
+    const int factor = 10;
+    sf::Vector2f velocity(0, 0);
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        velocity.y = -1;
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        velocity.y = 1;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        velocity.x = -1;
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        velocity.x = 1;
+    
+    float norm = 1.0f;
+    if (velocity.y && velocity.x)
+        norm = COS45;
+    double dt = deltaTime.asMicroseconds();
+    _shape.move(sf::Vector2f(velocity.x * norm * dt * _speed, velocity.y * norm * dt * _speed));
 }
 
 bool Player::loadTexture() {
