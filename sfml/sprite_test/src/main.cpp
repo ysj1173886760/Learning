e4d3f7b172d1563a44243754581a825de201812a
ysj1173886760 <1173886760@qq.com>
2021-08-19 12:33:52 +0800

    finish sprite test

--- sfml/sprite_test/src/main.cpp
+++ sfml/sprite_test/src/main.cpp
@@ -1,6 +1,30 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "hello world" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "test sprite");
+
+    sf::Texture texture;
+    if (!(texture.loadFromFile("../assets/survivor-idle_rifle_0.png"))) {
+        std::cerr << "warning: can not load texture" << std::endl;
+        return 0;
+    }
+    
+    sf::Vector2u textureSize = texture.getSize();
+    sf::RectangleShape rect(sf::Vector2f(textureSize.x / 2, textureSize.y / 2));
+    rect.setTexture(&texture);
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::EventType::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(rect);
+        window.display();
+    }
+
     return 0;
 }
