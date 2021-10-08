0d6006ad7cac6d9aa59fb384b614644e0793e0d0
ysj1173886760 <1173886760@qq.com>
2021-10-08 10:04:06 +0800

    update shader

--- graphics/LearnOpenGL/shader/vertexShader.glsl
+++ graphics/LearnOpenGL/shader/vertexShader.glsl
@@ -6,9 +6,11 @@ layout (location = 2) in vec2 aTexCoord;
 out vec3 ourColor; // 向片段着色器输出一个颜色
 out vec2 TexCoord;
 
+uniform mat4 transform;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = transform * vec4(aPos, 1.0);
     ourColor = aColor; // 将ourColor设置为我们从顶点数据那里得到的输入颜色
-    TexCoord = aTexCoord;
+    TexCoord = vec2(aTexCoord.x, 1.0 - aTexCoord.y);
 }
