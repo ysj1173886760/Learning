f04a734bb4f265b16875829abdb96f21ba823f38
ysj1173886760 <1173886760@qq.com>
2022-01-24 20:20:53 +0800

    upload rust code

---
+++ rust/geektime/day4/queryer/examples/covid.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use polars::prelude::*;
+use std::io::Cursor;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
+    let data = reqwest::get(url).await?.text().await?;
+
+    // 使用 polars 直接请求
+    let df = CsvReader::new(Cursor::new(data))
+        .infer_schema(Some(16))
+        .finish()?;
+
+    let filtered = df.filter(&df["new_deaths"].gt(500))?;
+    println!(
+        "{:?}",
+        filtered.select((
+            "location",
+            "total_cases",
+            "new_cases",
+            "total_deaths",
+            "new_deaths"
+        ))
+    );
+
+    Ok(())
+}
