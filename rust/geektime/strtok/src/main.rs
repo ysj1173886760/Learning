851e5aeca7c3c4a7b342d56c22b3be149a0c8870
ysj1173886760 <1173886760@qq.com>
2022-01-25 22:00:47 +0800

    update rust code

--- rust/geektime/strtok/src/main.rs
+++ rust/geektime/strtok/src/main.rs
@@ -1,4 +1,4 @@
-pub fn strtok<'a>(s: &'a mut &str, delimiter: char) -> &'a str {
+pub fn strtok<'a>(s: &mut &'a str, delimiter: char) -> &'a str {
     if let Some(i) = s.find(delimiter) {
         let prefix = &s[..i];
         let suffix = &s[(i + delimiter.len_utf8())..];
@@ -14,7 +14,11 @@ pub fn strtok<'a>(s: &'a mut &str, delimiter: char) -> &'a str {
 fn main() {
     let s = "hello world".to_owned();
     let mut s1 = s.as_str();
-    let hello = strtok(&mut s1, ' ');
+    let r = &mut s1;
+    let hello;
+    {
+        hello = strtok(r, ' ');
+    }
     println!("hello is: {}, s1: {}, s: {}", hello, s1, s);
 }
 
