f00296ae02306e90362f1f5c9d42eb834d7f4d5d
ysj1173886760 <1173886760@qq.com>
2022-01-25 21:31:37 +0800

    update rust code

---
+++ rust/geektime/strtok/src/main.rs
@@ -0,0 +1,23 @@
+pub fn strtok<'a>(s: &'a mut &str, delimiter: char) -> &'a str {
+    if let Some(i) = s.find(delimiter) {
+        let prefix = &s[..i];
+        let suffix = &s[(i + delimiter.len_utf8())..];
+        *s = suffix;
+        prefix
+    } else {
+        let prefix = *s;
+        *s = "";
+        prefix
+    }
+}
+
+fn main() {
+    let s = "hello world".to_owned();
+    let mut s1 = s.as_str();
+    let hello = strtok(&mut s1, ' ');
+    println!("hello is: {}, s1: {}, s: {}", hello, s1, s);
+}
+
+// s1的可变引用的生命周期应该只在strtok中
+// 如果将可变引用的生命周期与返回值的生命周期绑定，那么就相当于让s1的可变引用的生命周期与hello相同
+// 那么后一句中s1就会被认定冲突，因为还存在着对s1的可变引用
