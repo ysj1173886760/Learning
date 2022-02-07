e493f5d04127da90664e00ebbb4402f7871d2566
ysj1173886760 <1173886760@qq.com>
2022-02-07 21:46:21 +0800

    upload code

---
+++ rust/examples/where.rs
@@ -0,0 +1,22 @@
+use std::fmt::Debug;
+
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+// 这里需要一个 `where` 从句，否则就要表达成 `T: Debug`（这样意思就变了），
+// 或着改用另一种间接的方法。
+impl<T> PrintInOption for T where
+    Option<T>: Debug {
+    // 我们要将 `Option<T>: Debug` 作为约束，因为那是要打印的内容。
+    // 否则我们会给出错误的约束。
+    fn print_in_option(self) {
+        println!("{:?}", Some(self));
+    }
+}
+
+fn main() {
+    let vec = vec![1, 2, 3];
+
+    vec.print_in_option();
+}
